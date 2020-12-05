e74b9217fa49237ffaed20608def9f01abcdca09
RedBlueBird <jiefu.zhang1226@gmail.com>
2020-12-05 12:56:12 -0800

    2017 Silver

---
+++ cmake-build-debug/CMakeFiles/Problem1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Problem1.dir/2018_Jan/Silver/Problem1.cpp.obj"
+  "Problem1.exe"
+  "Problem1.exe.manifest"
+  "Problem1.pdb"
+  "libProblem1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Problem1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
