fdd936257d09d554899a3dfc544075b803617a50
RedBlueBird <jiefu.zhang1226@gmail.com>
2020-12-05 23:06:11 -0800

    2018 Jan Silver 1

---
+++ cmake-build-debug/CMakeFiles/1-Lifeguards.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1-Lifeguards.exe"
+  "1-Lifeguards.exe.manifest"
+  "1-Lifeguards.pdb"
+  "CMakeFiles/1-Lifeguards.dir/2018_Jan/Silver/1-Lifeguards.cpp.obj"
+  "lib1-Lifeguards.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1-Lifeguards.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
