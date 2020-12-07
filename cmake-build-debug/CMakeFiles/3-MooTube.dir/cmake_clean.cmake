d994765a20384d579fd7840af983e8c6acc356c5
RedBlueBird <jiefu.zhang1226@gmail.com>
2020-12-06 23:16:44 -0800

    2018 Feb Silver 1

---
+++ cmake-build-debug/CMakeFiles/3-MooTube.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3-MooTube.exe"
+  "3-MooTube.exe.manifest"
+  "3-MooTube.pdb"
+  "CMakeFiles/3-MooTube.dir/2018_Jan/Silver/3-MooTube.cpp.obj"
+  "lib3-MooTube.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3-MooTube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
