4870e22048a0ef84546c906a560bc8db9899587b
RedBlueBird <jiefu.zhang1226@gmail.com>
2020-12-06 16:13:01 -0800

    2018 Feb Silver 1

---
+++ 2018_Feb/Silver/RestStops.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main() {
+//    ofstream fout("reststops.out");
+//    ifstream fin("reststops.in");
+    ofstream fout("../output.txt");
+    ifstream fin("../input.txt");
+
+
+
+    return 0;
+}
