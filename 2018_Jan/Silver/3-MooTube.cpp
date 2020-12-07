04fda05dc3fa1024be80de95df47e5f14628e8ba
RedBlueBird <jiefu.zhang1226@gmail.com>
2020-12-06 16:09:40 -0800

    2018 Jan Silver 3

--- 2018_Jan/Silver/3-MooTube.cpp
+++ 2018_Jan/Silver/3-MooTube.cpp
@@ -3,30 +3,41 @@
 
 using namespace std;
 
-int main() {
-//    ofstream fout("rental.out");
-//    ifstream fin("rental.in");
-    ofstream fout("../output.txt");
-    ifstream fin("../input.txt");
+//Search for all videos that have not been visited with simple dfs
+void dfs(vector<pair<int,int>> graph[], vector<bool>& visited, int node, int limit, int& ans){
+    visited[node] = true;
+    for (int i = 0; i < graph[node].size(); i++){
+        if (visited[graph[node][i].first] == false && graph[node][i].second >= limit){
+            ans++;
+            dfs(graph, visited, graph[node][i].first, limit, ans);
+        }
+    }
+}
 
+int main() {
+    ofstream fout("mootube.out");
+    ifstream fin("mootube.in");
+//    ofstream fout("../output.txt");
+//    ifstream fin("../input.txt");
+
+    //Take Inputs
     int n, m;
     fin >> n >> m;
-    vector<int> graph[n];
+    vector<pair<int,int>> graph[n];
     for (int i = 0; i < n-1; i++){
         int a, b, c;
         fin >> a >> b >> c;
-        graph[a-1].push_back(c);
-        graph[b-1].push_back(c);
+        graph[a-1].push_back(make_pair(b-1,c));
+        graph[b-1].push_back(make_pair(a-1, c));
     }
+
+    //Dfs to search all videos with minimum relevance, output the ans along the way.
     for (int i = 0; i < m; i++){
         int a, b;
         fin >> a >> b;
         int ans = 0;
-        for (int j: graph[b-1]){
-            if (j >= a){
-                ans++;
-            }
-        }
+        vector<bool> visited(n, false);
+        dfs(graph, visited, b-1, a, ans);
         fout << ans << "\n";
     }
 
