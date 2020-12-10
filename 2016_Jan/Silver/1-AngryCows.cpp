ed4704905890965a5752fd5c9e3762d16a38aea0
RedBlueBird <jiefu.zhang1226@gmail.com>
2020-12-09 19:26:50 -0800

    2016 Jan Silver 2

---
+++ 2016_Jan/Silver/1-AngryCows.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main() {
+//    ofstream fout("angry.out");
+//    ifstream fin("angry.in");
+    ofstream fout("../output.txt");
+    ifstream fin("../input.txt");
+
+    //Take inputs
+    int n, k;
+    fin >> n >> k;
+    vector<int> cows(n);
+    for (int i = 0; i < n; i++){
+        fin >> cows[i];
+    }
+
+    vector<int> dividers(k);
+    for (int i = 0; i < k; i++){
+
+    }
+
+    return 0;
+}
--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -57,4 +57,6 @@ set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/USACO_contests.dir/DependInfo.cmake"
   "CMakeFiles/2-SnowBoots.dir/DependInfo.cmake"
   "CMakeFiles/3-Teleportation.dir/DependInfo.cmake"
+  "CMakeFiles/1-AngryCows.dir/DependInfo.cmake"
+  "CMakeFiles/2-SubsequencesSummingToSevens.dir/DependInfo.cmake"
   )
